869de49c26db0ffd520f4b2d79aacb5f4b1d4faf
leo848 <leonardo.blume@gymnasium-essen-werden.de>
2020-09-10 22:25:10 +0200

    INITIAL

---
+++ car.js
@@ -0,0 +1,75 @@
+class Car {
+	constructor (x, y, angle, image) {
+		this.pos = createVector(x, y);
+		this.angle = angle;
+		this.image = image;
+
+		this.acc = 0;
+		this.vel = createVector(0, 0);
+	}
+
+	show () {
+		push();
+		translate(this.pos.x, this.pos.y);
+		rotate(this.angle);
+
+		image(this.image, 0, 0, 100, 100);
+		pop();
+	}
+
+	update () {
+		if (keyIsDown(87)) {
+			// 87 is key code for 'w'
+			if (
+				arraysEqual(
+					this.getCurrentColor(),
+					[ 88, 88, 88 ],
+				)
+			)
+				this.acc += 0.1;
+			else if (
+				arraysEqual(
+					this.getCurrentColor(),
+					[ 255, 255, 255 ],
+				)
+			) {
+				this.acc += 0.2;
+			} else {
+				this.acc = 0.2;
+			}
+		} else {
+			if (this.acc - 0.1 >= 0) {
+				this.acc -= 0.1;
+			} else {
+				this.acc = 0;
+			}
+		}
+
+		this.vel = getVectorFromMagnitudeAndDirection(
+			this.acc,
+			(this.angle + 90) % 360, // this is needed because in p5 0° is right.
+		);
+		if (keyIsDown(65)) {
+			//a
+			this.angle -= 3;
+		}
+
+		if (keyIsDown(68)) {
+			//s
+			this.angle += 3;
+		}
+
+		this.pos.add(this.vel);
+	}
+
+	getCurrentColor () {
+		let x = Math.floor(this.pos.x);
+		let y = Math.floor(this.pos.y);
+		let index = (x + y * width) * 4;
+		return [
+			pixels[index],
+			pixels[index + 1],
+			pixels[index + 2],
+		];
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,59 @@
+let carImage, car, track;
+
+function preload (){
+	carImage = loadImage('auto.png');
+	track = loadImage('strecke1.png');
+}
+
+function setup (){
+	createCanvas(
+		min(windowWidth, windowHeight),
+		min(windowWidth, windowHeight),
+	);
+	background(20);
+	imageMode(CENTER);
+	angleMode(DEGREES);
+	noSmooth();
+	car = new Car(250, 550, 180, carImage);
+}
+
+function draw (){
+	loadPixels();
+	image(
+		track,
+		width / 2,
+		height / 2,
+		width,
+		height,
+	);
+	fill(255, 0, 0);
+	rect(200, 200, 40, 40);
+	car.update();
+	car.show();
+}
+
+function getVectorFromMagnitudeAndDirection ( // this is needed for the car movement.
+	mag,
+	dir,
+){
+	return createVector(
+		mag * cos(dir),
+		mag * sin(dir),
+	);
+}
+
+function arraysEqual (a, b){
+	if (a === b) return true;
+	if (a == null || b == null) return false;
+	if (a.length !== b.length) return false;
+
+	// If you don't care about the order of the elements inside
+	// the array, you should sort both arrays here.
+	// Please note that calling sort on an array will modify that array.
+	// you might want to clone your array first.
+
+	for (var i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
+}
